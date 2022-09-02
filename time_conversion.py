c1526391569b2e93b6cbbba3b8d33b594eef4f4d
Ibnu Azam A <44658303+ibnuazam@users.noreply.github.com>
2022-09-02 16:05:05 +0700

    add days

--- time_conversion.py
+++ time_conversion.py
@@ -1,7 +1,8 @@
-#convert seconds to hours, minutes, seconds
+#convert seconds to days, hours, minutes, seconds
 import math
 input_seconds = int(input("Enter seconds: "))
+days = math.floor(input_seconds / 86400)
 hours = math.floor(input_seconds / 3600)
 minutes = math.floor((input_seconds - (hours * 3600)) / 60)
-seconds = input_seconds - (hours * 3600) - (minutes * 60)
-print(hours, "jam :", minutes, "menit :", seconds, "detik")
+seconds = input_seconds - (days * 86400) - (hours * 3600) - (minutes * 60)
+print(days, "hari :", hours, "jam :", minutes, "menit :", seconds, "detik")
