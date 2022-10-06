a7931dbcc0f49d52812aecb1364e9e19f305f529
Ibnu Azam A <44658303+ibnuazam@users.noreply.github.com>
2022-10-06 14:43:04 +0700

    delete old code files

--- old_code/time_conversion.py
+++
@@ -1,8 +0,0 @@
-#convert seconds to days, hours, minutes, seconds
-import math
-input_seconds = int(input("Enter seconds: "))
-days = math.floor(input_seconds / 86400)
-hours = math.floor((input_seconds - (days * 86400))/ 3600)
-minutes = math.floor(((input_seconds - (days * 86400)) - (hours * 3600)) / 60)
-seconds = input_seconds - (days * 86400) - (hours * 3600) - (minutes * 60)
-print(days, "hari :", hours, "jam :", minutes, "menit :", seconds, "detik")
